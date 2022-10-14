d89dcb4cfac628d00dc98dced00a75e0ee960f8d
srite <me@srit.xyz>
2022-10-14 14:10:40 +0300

    Добавил пример конфига

---
+++ config.example.ts
@@ -0,0 +1,12 @@
+export default {
+	mongodb: "",
+	discord: {
+		id: "",
+		secret: "",
+		token: ""
+	},
+	redirectUrl: "http://localhost:3000/api/auth",
+	url: "http://localhost:3001",
+	port: 3001,
+	admins: ["525748937349529602"]
+}
