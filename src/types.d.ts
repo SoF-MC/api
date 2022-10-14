a3cbe7c10c32c24710248080a5423ba21683100f
srite <me@srit.xyz>
2022-10-14 14:15:47 +0300

    Изменения в типах

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,65 +1,87 @@
-import { FastifyRequest, FastifyReply, HTTPMethods } from "fastify"
+import { FastifyRequest, FastifyReply, HTTPMethods, FastifySchema, RouteGenericInterface, FastifyInstance } from 'fastify';
 
 export interface UserData {
-    id: string,
-    username: string,
-    discriminator: string,
-    avatar: string,
-    tag: string
+	id: string,
+	username: string,
+	discriminator: string,
+	avatar: string,
+	tag: string
 }
 
 export interface CardData {
-    ownerId: string,
-    name: string,
-    balance: number
+	ownerId: string,
+	name: string,
+	balance: number
 }
 
 export interface ActionData {
-    actionId: string,
-    createdAt: string,
-    userId: string,
-    recieverId?: string,
-    amount: number
+	createdAt?: string,
+	userId: string,
+	recieverId?: string,
+	amount: number
+}
+
+export interface TicketData {
+	channelId: string,
+	userId: string,
+	data: {
+		nick: string,
+		age: number,
+		desc: string
+	},
+	reason?: string,
+	closed: boolean
+}
+
+export interface TransferOptions {
+	recieverId: string,
+	amount: number
 }
 
 export interface ActionCreateOptions {
-    userId: string,
-    recieverId?: string,
-    amount: number
+	userId: string,
+	recieverId?: string,
+	amount: number
 }
 
 export interface AppOptions { }
 
 export interface AppRoute {
-    run: (request: FastifyRequest, reply: FastifyReply) => any
-    route: string,
-    method: HTTPMethods
+	run: (request: FastifyRequest<any>, reply: FastifyReply<any>) => any | Promise<any>,
+	schema?: FastifySchema,
+	route: string,
+	register: (app: FastifyInstance) => any
+	method: HTTPMethods,
+}
+
+export interface DefaultHeaders {
+	authorisation: string
 }
 
 //Discord api raw things
 
 export interface DiscordRawApiResponse {
-    data: DiscordRawApiData
+	data: DiscordRawApiData
 }
 
 export type DiscordUserLocale =
-    "da" | "de" | "en-GB" | "en-US" | "es-ES" | "fr" | "hr" | "it" | "lt" | "hu" | "nl" | "no" | "pl" | "pt-BR" | "ro" | "fi" | "sv-SE" | "vi" | "tr" | "cs" | "el" | "bg" | "ru" | "uk" | "hi" | "th" | "zh-CN" | "ja" | "zh-TW" | "ko"
+	"da" | "de" | "en-GB" | "en-US" | "es-ES" | "fr" | "hr" | "it" | "lt" | "hu" | "nl" | "no" | "pl" | "pt-BR" | "ro" | "fi" | "sv-SE" | "vi" | "tr" | "cs" | "el" | "bg" | "ru" | "uk" | "hi" | "th" | "zh-CN" | "ja" | "zh-TW" | "ko"
 
 export type DiscordUserPremiumType = 0 | 1 | 2
 
 export interface DiscordRawApiData {
-    id: string,
-    username: string,
-    discriminator: string,
-    avatar: string,
-    bot?: boolean,
-    system?: boolean,
-    mfa_enabled?: boolean,
-    banner: string,
-    accent_color: number
-    locale: DiscordUserLocale,
-    flags: number,
-    premium_type: DiscordUserPremiumType,
-    public_flags: number
+	id: string,
+	username: string,
+	discriminator: string,
+	avatar: string,
+	bot?: boolean,
+	system?: boolean,
+	mfa_enabled?: boolean,
+	banner: string,
+	accent_color: number
+	locale: DiscordUserLocale,
+	flags: number,
+	premium_type: DiscordUserPremiumType,
+	public_flags: number
 }
 
